5985aa8039832b88b3c0f6de7867e4b88a05477a
TejdeepTheAlpha <103597763+TejdeepTheAlpha@users.noreply.github.com>
2023-08-31 18:06:56 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+
+// add SDK Firebase
+var firebaseConfig = {
+
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.database();
+const firebaseConfig = {
+    apiKey: "AIzaSyD8nw-yGidozgIBdDwhAd9Rvts1YQpaa-I",
+    authDomain: "team-voting-app-d825b.firebaseapp.com",
+    databaseURL: "https://team-voting-app-d825b-default-rtdb.firebaseio.com",
+    projectId: "team-voting-app-d825b",
+    storageBucket: "team-voting-app-d825b.appspot.com",
+    messagingSenderId: "326839509537",
+    appId: "1:326839509537:web:a290d1e9c6579dbbe37960"
+  };
+  
